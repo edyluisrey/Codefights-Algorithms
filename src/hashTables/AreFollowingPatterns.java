38494ce3aa27cad3f2791c2c13e407e061eb5acf
edyluisrey <eaguirre378@gmail.com>
2017-07-25 22:17:14 -0500

    Solution for: Following pattern

--- src/hashTables/AreFollowingPatterns.java
+++ src/hashTables/AreFollowingPatterns.java
@@ -5,15 +5,15 @@ import java.util.Map;
 
 public class AreFollowingPatterns {
 	boolean areFollowingPatterns(String[] strings, String[] patterns) {
-	     Map<String, String> map = new HashMap<>();
-	     for(int i=0; i< strings.length; i++){
-	          if(map.containsKey(patterns[i])){
-	               if(!map.get(patterns[i]).equals(strings[i])) return false;
-	          }else if(map.containsValue(strings[i])){
-	               return false;
-	          }
-	          map.put(patterns[i], strings[i]);
-	     }
-	     return true;
+		     Map<String, String> map = new HashMap<>();
+		     for(int i=0; i< strings.length; i++){
+		          if(map.containsKey(patterns[i])){
+		               if(!map.get(patterns[i]).equals(strings[i])) return false;
+		          }else if(map.containsValue(strings[i])){
+		               return false;
+		          }
+		          map.put(patterns[i], strings[i]);
+		     }
+		     return true;
 	}
 }
